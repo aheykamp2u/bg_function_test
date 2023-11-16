a8e2cb7773da678bdd607266dc266f064b314a42
Andrew Heykamp <aheykamp@2u.com>
2023-11-16 11:20:22 -0500

    started fleshing out background job

--- netlify/functions/algolia_indexing-background.mjs
+++ netlify/functions/algolia_indexing-background.mjs
@@ -1,3 +1,22 @@
+import algoliasearch from 'algoliasearch';
+
 export default async (req, context) => {
-  console.log('done');
+  console.log('starting job');
+  console.log('generating objects');
+
+  const objects = [];
+  for(var i = 0; i < 100; i++) {
+    objects.push(generate_object(i));
+  };
+
+  console.log(objects);
 };
+
+
+
+const generate_object = (index) => {
+  return {
+    objectID: index,
+    name: `test-object-${index}`,
+  }
+}
